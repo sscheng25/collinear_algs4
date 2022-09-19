dbf75f3ee20f3011a5681807b9b70aa85f5870e8
Sisun Cheng <997723678@qq.com>
2022-09-18 23:31:53 -0400

    Initial commit

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    private int num;
+
+    public BruteCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 points
+    }
+
+    public int numberOfSegments() {
+        // the number of line segments
+        return num;
+    }
+
+    public LineSegment[] segments() {
+        // the line segments
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
