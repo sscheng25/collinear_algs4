acf8317f58632ad757c0b5a4f2c38424510cab69
Sisun Cheng <997723678@qq.com>
2022-09-18 23:48:56 -0400

    create FastCollinearPoints

---
+++ FastCollinearPoints.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 or more points
+    }
+
+    public int numberOfSegments() {
+        // the number of line segments
+    }
+
+    public LineSegment[] segments() {
+        // the line segments
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
